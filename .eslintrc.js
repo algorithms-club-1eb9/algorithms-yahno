a4d62fb22a90a7007495fb9d8789a70818300abb
vasyayahno1997@gmail.com <gerry@Vasyas-MacBook-Pro.local>
2018-03-04 13:24:57 +0200

    pass first test

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "no-tabs": 0,
+        "newline-per-chained-call": 0,
+        "indent": 0,
+        "no-undef": 0,
+        "no-return-assign": 0,
+        "no-underscore-dangle": 0,
+        "prefer-destructuring": 0,
+        "class-methods-use-this": 0,
+        "linebreak-style": 0,
+        "no-param-reassign": 0,
+        "arrow-parens": 0,
+        "no-console": 0,
+    }
+};
